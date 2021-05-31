3352b37c7797d3b19c76cae93378439100df3433
mm--takada <nn.takada0203@gmail.com>
2021-05-31 14:35:23 +0900

    change line9 & line74

--- calculator.java
+++ calculator.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
-public class dentaku {
+public class calculator {
   public static void main(String[] args) {
     System.out.print("f:");
     Scanner sc = new Scanner(System.in);
